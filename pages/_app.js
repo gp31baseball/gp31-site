2e04e59fbeb9a999178630d18dad5346a141a63e
gp31baseball <james.snell@darkvisiontech.com>
2025-10-22 08:53:38 -0500

    fix scroll bar crash

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,11 @@
 import "../styles/globals.css";
 import Navbar from "../components/Navbar";
-import ScrollTop from "../components/ScrollTop";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Navbar />
       <Component {...pageProps} />
-      <ScrollTop />
-    </>
+          </>
   );
 }
